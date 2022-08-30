1a30939e0088105346b03dbe7ea61da5e5f6e715
szlabingerVA <106733710+szlabingerVA@users.noreply.github.com>
2022-08-30 16:37:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -410,7 +410,7 @@ if selected_hor == "Overview":
                         "label": {
                             "color": '#0082B4',
                             "formatter": '{b}\n{c}%',
-                            "fontSize":15,
+                            "fontSize":16,
                         },
                         "labelLine": {
                             "lineStyle": {
