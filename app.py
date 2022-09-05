079bd1c541bcbf3a08b4fdfa2fb0d3bb0a28a5e1
szlabingerVA <106733710+szlabingerVA@users.noreply.github.com>
2022-09-05 10:31:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3197,4 +3197,4 @@ if selected_hor == "Stückliste":
 
         #Bei fehlenden Input:
         except ValueError:
-            st.markdown('<p style="text-align: center;color: #FFF;font-size:20px">Bitte uploaden Sie die Analyse-Dateien!</p>', unsafe_allow_html=True)
+            st.markdown('<p style="text-align: center;color: #FFF;font-size:20px">Bitte wählen Sie einen Zeitraum!</p>', unsafe_allow_html=True)
